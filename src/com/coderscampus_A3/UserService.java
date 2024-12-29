77967cf143feab72bd78d82ca29b8fd469016715
S-create1900 <saphron_m@yahoo.com>
2024-12-28 19:26:13 -0700

    Updated UserService
    Changed ArrayList to a native array of Users.

--- src/com/coderscampus_A3/UserService.java
+++ src/com/coderscampus_A3/UserService.java
@@ -3,49 +3,53 @@ package com.coderscampus_A3;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class UserService {
-	private List<UserPOJO> users;
-	private int userCount;
-	private String csvFilePath;
+    
+    private UserPOJO[] users; // native array for users
+    private int userCount; 
+    private String csvFilePath;
 
-	public UserService() {
-		this.csvFilePath = "src/data.txt";
-		this.users = new ArrayList<>();
-		this.userCount = 0;
-		loadUsersFromCsv();
-	}
+    public UserService() {
+        this.csvFilePath = "src/data.txt"; 
+        this.users = new UserPOJO[4]; 
+        this.userCount = 0;
+        loadUsersFromCsv(); 
+    }
 
-	public UserPOJO authenticate(String username, String password) {
-		for (UserPOJO user : users) {
-			if (user.getUsername().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
-				return user;
-			}
-		}
-		return null;
-	}
+    public UserPOJO authenticate(String username, String password) {
+        for (int i = 0; i < userCount; i++) { 
+            UserPOJO user = users[i];
+            if (user.getUsername().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
+                return user; 
+            }
+        }
+        return null; 
+    }
 
-	private void loadUsersFromCsv() {
-		try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
-			String line;
-			while ((line = br.readLine()) != null) {
-				String[] data = line.split(",");
-				if (data.length >= 3) {
-					String email = data[0].trim();
-					String password = data[1].trim();
-					String name = data[2].trim();
-					addUser(new UserPOJO(email, password, name));
-				}
-			}
-		} catch (IOException e) {
-			System.err.println("Error loading users from CSV file: " + e.getMessage());
-		}
-	}
+    private void loadUsersFromCsv() {
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(csvFilePath))) {
+            String line;
+            while ((line = fileReader.readLine()) != null) {
+                String[] lineData = line.split(",");
+                if (lineData.length >= 3) { 
+                    String username = lineData[0].trim();
+                    String password = lineData[1].trim();
+                    String name = lineData[2].trim();
+                    addUser (new UserPOJO(username, password, name)); 
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error loading users from CSV file: " + e.getMessage());
+        }
+    }
 
-	public void addUser(UserPOJO user) {
-		users.add(user);
-		userCount++;
-	}
+    public void addUser (UserPOJO user) {
+        if (userCount < users.length) { 
+            users[userCount] = user; 
+            userCount++; 
+        } else {
+            System.err.println("User  array is full. Cannot add more users.");
+        }
+    }
 }
